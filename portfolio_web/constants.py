47a64880f4232e74d1be9eefa5bf80417b2b0097
bryan <bryanvivanco97@outlook.com>
2025-04-20 21:52:35 -0500

    Update projects

--- portfolio_web/constants.py
+++ portfolio_web/constants.py
@@ -10,6 +10,8 @@ CV_URL = "https://docs.google.com/document/d/1COvjcCJE4GXUlkkSa9U6CrofLQPBkKWM/e
 CV_EN_US_URL = "https://docs.google.com/document/d/1-HQzm3XRZhPyWru5ViaSeCmuRuVj-9RO/edit?usp=sharing&ouid=117976139180001148882&rtpof=true&sd=true"
 
 
+MY_GITHUB = "https://github.com/bryanvivancos"
+CATQUIZ_WEB_V2 = "https://github.com/bryanvivancos/catquizweb-v2"
 FORMULARIOWEB_URL = "https://github.com/bryanvivancos/formularioweb_a_pdf"
 GENERATINGDATA_URL = "https://github.com/bryanvivancos/python_crash_course/tree/main/generating_data/chapter_15"
 GETTINDJANGO_URL = "https://github.com/bryanvivancos/python_crash_course/tree/main/web_applications"
@@ -25,10 +27,11 @@ MAS_CERTIFICADOS = "https://drive.google.com/drive/folders/1EbKWVsqkyQ4OUvZwf5ZE
 
 ##############################
 
-API_PRACTICO_URL = "https://drive.google.com/file/d/1dI9_KsUwhDqZ0SlyLDTcl_A8aWJFN7Pz/view?usp=sharing"
-INTRO_BACKEND_URL = "https://drive.google.com/file/d/1E7SvIIDxxe9-MIvXdCfREDsPkDZjUidO/view?usp=sharing"
-DOM_URL = "https://drive.google.com/file/d/1Z-uic7TwvXKRslZlmc3grf-6kxoxkIl4/view?usp=sharing"
-WEB_COMPONENTS_URL = "https://drive.google.com/file/d/1HWUNXAzvusH7xV4jZ36yOpyZCdfejkB8/view?usp=sharing"
+REACT_JS_PLATZI = "https://drive.google.com/file/d/1y3RMCUB6ECsQ2q52AffRIFzlWK1PXeDb/view?usp=sharing"
+API_PRACTICO_URL_PLATZI = "https://drive.google.com/file/d/1dI9_KsUwhDqZ0SlyLDTcl_A8aWJFN7Pz/view?usp=sharing"
+INTRO_BACKEND_URL_PLATZI = "https://drive.google.com/file/d/1E7SvIIDxxe9-MIvXdCfREDsPkDZjUidO/view?usp=sharing"
+DOM_URL_PLATZI = "https://drive.google.com/file/d/1Z-uic7TwvXKRslZlmc3grf-6kxoxkIl4/view?usp=sharing"
+WEB_COMPONENTS_URL_PLATZI = "https://drive.google.com/file/d/1HWUNXAzvusH7xV4jZ36yOpyZCdfejkB8/view?usp=sharing"
 DSFB_URL = "https://drive.google.com/file/d/1wwGnsUsofVXpkxNwbygFpsABEJjYh_eA/view?usp=sharing"
 CERTCIETSI_URL = "https://drive.google.com/file/d/1IY8a3koFBj9LXOB8GCmOxS-8oduj26Lj/view?usp=sharing"
 NETNDGLINUX_URL = "https://drive.google.com/file/d/1izHdLPAQCJFODG4YwkWHxt5iDhoqjEMs/view?usp=sharing"
